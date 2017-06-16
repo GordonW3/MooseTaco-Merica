7da02275234cab9fa77ad4552edcd8505a123b18
Gordon <gordon.wallac3@yahoo.com>
2017-06-16 11:01:43 -0600

    fun little project

---
+++ db/migrate/20170616152440_create_chats.rb
@@ -0,0 +1,11 @@
+class CreateChats < ActiveRecord::Migration[5.0]
+  def change
+    create_table :chats do |t|
+      t.string :name
+      t.text :image
+      t.text :chat
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |chat|
+	Chat.create!(
+		name: "lobster bob",
+		image: "http://daleholloweaglecam.com/wp-content/uploads/2015/12/eagleface.jpg",
+		chat: "on the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untraveled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+		)
+end
+
+puts "10 Chats items created"
